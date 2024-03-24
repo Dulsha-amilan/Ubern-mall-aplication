38038d9fc7a25473d7c6160c55d6e6565822e633
Senavirathna W.H.D.A <dulsharulzzz@gmail.com>
2024-03-24 23:44:23 +0530

    add a new barach

--- frontend/src/Janidu/navbar.js
+++ frontend/src/Janidu/navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+//add new one
 const Header = () => {
   return (
     <header class="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white text-sm py-4 dark:bg-gray-800">
